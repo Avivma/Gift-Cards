ca499f5649f36c02b94361438773a850ebafb6c3
aviv <avivmaor9@gmail.com>
2022-03-07 17:50:15 +0200

    preference screens - minor change (adjust method)

--- app/src/main/java/com/example/composefirsttry/preferencescreens/fragments/PrefViewModel.kt
+++ app/src/main/java/com/example/composefirsttry/preferencescreens/fragments/PrefViewModel.kt
@@ -14,25 +14,20 @@ import com.example.composefirsttry.preferencescreens.miscellaneous.PrefEvents
 import com.example.composefirsttry.preferencescreens.miscellaneous.SHARD_PREF_NAME
 import com.example.composefirsttry.preferencescreens.widget.TriCheckBox
 
-class PrefViewModel (application: Application) : AndroidViewModel(application) {
+class PrefViewModel(application: Application) : AndroidViewModel(application) {
     private val sp: SharedPreferences = application.getSharedPreferences(SHARD_PREF_NAME, Context.MODE_PRIVATE)
 
-    fun init(): ArrayList<ParentPrefItem> {
-//        L.i("PrefViewModel - init")
-        val prefItems: ArrayList<ParentPrefItem> = arrayListOf()
-        ParentCategoryEnum.values().toCollection(ArrayList()).forEach { category ->
+    fun init(): ArrayList<ParentPrefItem> =
+        ParentCategoryEnum.values().toCollection(ArrayList()).map { category ->
             val state = sp.getInt(category.text, TriCheckBox.UNCHECKED)
-            prefItems.add(ParentPrefItem(category, state))
-        }
-
-        return prefItems
-    }
+            ParentPrefItem(category, state)
+        }.toCollection(ArrayList())
 
     fun updateState(parentItem: ParentPrefItem) {
         val editor = sp.edit()
         if (parentItem.state == TriCheckBox.CHECKED || parentItem.state == TriCheckBox.UNCHECKED) {
             val childState: Boolean = parentItem.state == TriCheckBox.CHECKED
-            val childItems: ArrayList<CategoryEnum> = when(parentItem.prefType) {
+            val childItems: ArrayList<CategoryEnum> = when (parentItem.prefType) {
                 ParentCategoryEnum.SWEETS -> CategoryEnum.getSweets()
                 ParentCategoryEnum.SALTS -> CategoryEnum.getSalts()
             }
